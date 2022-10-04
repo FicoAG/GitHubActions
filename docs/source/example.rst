c587036d0cdb375ede49f1a1411e180ae81cdddc
Fico Aguiar <fico.aguiar@bitingbit.com>
2022-10-04 20:40:01 +0100

    pytest docs test

--- docs/source/example.rst
+++ docs/source/example.rst
@@ -5,3 +5,4 @@ Esto es una prueba
 
 .. code-block:: python
     get_numerical_features(df)
+    return list(df.select_dtypes(include=[np.number]).columns)
--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -1,9 +1,23 @@
 """
-This module contains helpers for data processing.
+Este módulo contiene helpers para el preprocesamiento de datos
 """
-
-import numpay as np
+import numpy as np
 
 
 def get_numerical_features(df):
-    return df.select_dtypes(include=[np.number]).columns
+    """get_numerical features devuelve una lista
+    con el nombre de las columnas que contienen datos
+    de tipo numérico
+    
+    Parámetros
+    ----------
+    df: dataframe
+    Ejemplos
+    --------
+    >>> from modeltools.preprocessing import get_numerical_features
+    >>> import pandas as pd
+    >>> df = pd.DataFrame({"a":[1]})
+    >>> get_numerical_features(df)
+    ['a']
+    """
+    return list(df.select_dtypes(include=[np.number]).columns)
